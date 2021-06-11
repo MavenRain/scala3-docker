3d11b0fb1d8c71651c1ed318b23bf01099b8423f
Onyeka Obi <Onyeka.Obi@gmail.com>
2021-06-11 02:10:48 -0700

    TupleExtensions

--- src/main/scala/io/github/mavenrain/Main.scala
+++ src/main/scala/io/github/mavenrain/Main.scala
@@ -1,2 +1,8 @@
+package io.github.mavenrain
+
+import io.github.mavenrain.removeElem
+
 @main
-def run = println("Hello world!")
+def run =
+  println("Hello world!")
+  println(("Hi", true, 2).removeElem[Int]._2)
